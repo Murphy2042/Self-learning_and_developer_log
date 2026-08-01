2be9faaa68fc64d7f3914a8d4179db2a19511c0f
Simon M. <jiamofei7@gmail.com>
2026-08-01 22:16:43 +0800

    Add basic price charge calculation in C
    Implement basic C program to calculate charge based on input price.

---
+++ trying_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int price=0
+
+  printf("please input the price")
+  scanf("%d",&price)
+
+  int charge= 100-price
+  printf("we will charge you for %d dollars", charge)
+  return 0
+}
+
+//I learn the basic structure of C programming, including decleration, class, printf, scanf, int.
+/* ohh, I find that the comment in C programming is not like a # in python
+interesting! */
